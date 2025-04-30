49040efc0330d706cb97f13dc09dd2d0d34ff596
arjun29-devops <arjuncs0495@gmail.com>
2025-04-30 14:58:28 +0530

    Add files via upload

---
+++ services/voyage-fleet-monitoring-api/staging-new/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {
+  profile = "hull-dev-test"
+  region  = "ap-south-1"
+}
+terraform {
+  backend "s3" {
+    bucket  = "ocean-eye-tfstate-stag"
+    key     = "services/voyage-fleet-monitoring-api/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "hull-dev-test"
+  }
+}
+#resource "aws_ecs_cluster" "this" {
+ # name = "hull-common-apps-ecs-cluster-stg"
+#}
+module "ecs" {
+  cluster_name              = "ocean-eye-common-apps-ecs-cluster-staging"
+  source                    = "../../../modules/services/ecs-existing-alb-app"
+  alb_security_group_id     = "sg-0170cb4558ea4b1ed"
+  container_image           = "nginx"
+  created_by                = "DevOps Team"
+  env                       = local.env
+  project_name              = local.project_name
+  service_name              = local.project_name
+  target_group_health_check = "/api/vf/Health/HealthCheck"
+  vpc_id                    = "vpc-00fd8e939e2206691"
+  ecs_subnet_ids            = ["subnet-0235b7c1da2864fab", "subnet-003d016854abdfc46"]
+  alb_listener_arn          = "arn:aws:elasticloadbalancing:ap-south-1:676050803833:listener/app/ocean-eye-staging/fc45705512ff4a3b/87a4cabe9b5dac5e"
+  alb_path_based_listener   = "/api/vf/*"
+ # depends_on                = [aws_ecs_cluster.this]
+  create_ecr_repo           = true
+}
+
+locals {
+  project_name = "voyage-fleet-monitoring-api"
+  env          = "stg"
+}
+
---
+++ services/voyage-optimization-main-api/qa/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  profile = "hull-dev-test"
+  region  = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "hull-devops-tfstate-qa"
+    key     = "services/voyage-optimization-main-api/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "hull-dev-test"
+  }
+}
+
+ resource "aws_ecs_cluster" "this" {
+  name = "voyage-optimization-common-apps-ecs-cluster-qa"
+}
+module "ecs" {
+  cluster_name              = aws_ecs_cluster.this.name
+  source                    = "../../../modules/services/ecs-existing-alb-app"
+  alb_security_group_id     = "sg-01d99cb2da3d19872"
+  container_image           = "nginx"
+  created_by                = "DevOps Team"
+  env                       = local.env
+  project_name              = local.project_name
+  service_name              = local.project_name
+  target_group_health_check = "/api/vm/Health/HealthCheck"
+  vpc_id                    = "vpc-0750e62e513aaeb9e"
+  ecs_subnet_ids            = ["subnet-0a1d7594a83eb7b5a", "subnet-0c15e2404500f2272"]
+  alb_listener_arn          = "arn:aws:elasticloadbalancing:ap-south-1:676050803833:listener/app/hull-qa-alb/fb85a9036ab20d70/d5f98578c9044689"
+  alb_path_based_listener   = "/api/vm/*"
+  depends_on                = [aws_ecs_cluster.this]
+  create_ecr_repo           = true
+}
+
+locals {
+  project_name = "voyage-optimization-main-api"
+  env          = "qa"
+}
