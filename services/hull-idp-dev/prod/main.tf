cacff601aecfee6fc5790d538e718c5a0c84d890
arjun29-devops <arjuncs0495@gmail.com>
2025-04-30 14:45:03 +0530

    Add files via upload

---
+++ services/hull-idp-dev/prod/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  profile = "ocean-eye-prod"
+  region  = "ap-south-1"
+}
+terraform {
+  backend "s3" {
+    bucket  = "ocean-eye-tfstate-prod"
+    key     = "services/ocean-eye-admin-api-prod/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "ocean-eye-prod"
+  }
+}
+#resource "aws_ecs_cluster" "this" {
+ # name = "ocean-eye-apps-cluster-prod"
+#}
+module "ecs" {
+  cluster_name              = "ocean-eye-apps-cluster-prod"
+  source                    = "../../../modules/services/ecs-existing-alb-app"
+  alb_security_group_id     = "sg-071a0601e88ca5f7b"
+  container_image           = "nginx"
+  created_by                = "DevOps Team"
+  env                       = local.env
+  project_name              = local.project_name
+  service_name              = local.project_name
+  target_group_health_check = "/ping/health"
+  vpc_id                    = "vpc-066a62f5a37050652"
+  ecs_subnet_ids            = ["subnet-058c5690d99ddcbe9", "subnet-02c8313cc63b86a8c", "subnet-0f00cee1b13c62af1"]
+  alb_listener_arn          = "arn:aws:elasticloadbalancing:ap-south-1:339713047016:listener/app/ocean-eye-prod-alb/b18f797375cf46c1/ee684618b10e41fd"
+  alb_path_based_listener   = "/health/ping/*"
+ # depends_on                = [aws_ecs_cluster.this]
+  create_ecr_repo           = true
+}
+locals {
+  project_name = "ocean-eye-idp-admin-api"
+  env          = "prod"
+}
+
---
+++ services/hull-propeller-api/develop/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  profile = "hull-dev-test"
+  region  = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "hull-devops-tfstate-dev"
+    key     = "services/hull-propeller-api-dev/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "hull-dev-test"
+  }
+}
+
+#resource "aws_ecs_cluster" "this" {
+ # name = "hull-common-apps-ecs-cluster-dev"
+#}
+module "ecs" {
+  cluster_name              = "hull-common-apps-ecs-cluster-dev"
+  source                    = "../../../modules/services/ecs-existing-alb-app"
+  alb_security_group_id     = "sg-070fb1eefe055c38c"
+  container_image           = "nginx"
+  created_by                = "DevOps Team"
+  env                       = local.env
+  project_name              = local.project_name
+  service_name              = local.project_name
+  target_group_health_check = "/v1/v1/health"
+  vpc_id                    = "vpc-0b0c5cb770bdaf445"
+  ecs_subnet_ids            = ["subnet-0968b8383f8c092ce", "subnet-01aaa66ae8cb1a030"]
+  alb_listener_arn          = "arn:aws:elasticloadbalancing:ap-south-1:676050803833:listener/app/hull-dev-alb/c00b254950895b6d/9b7a638f7b7f1d7d"
+  alb_path_based_listener   = "/v1/v1/*"
+ #depends_on                = [aws_ecs_cluster.this]
+  create_ecr_repo           = true
+}
+
+locals {
+  project_name = "hull-propeller-api"
+  env          = "dev"
+}
